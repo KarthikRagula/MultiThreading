d89192b1ae774d10a04f95496687a1503986b385
KarthikRagula <karthik.ragula@wavemaker.com>
2025-02-07 12:29:19 +0530

    written output to the file and calculated time for different number of consumers

--- src/main/java/org/multithreading/producerconsumerproblemwordsearch/service/WordSearchWithProducerConsumer.java
+++ src/main/java/org/multithreading/producerconsumerproblemwordsearch/service/WordSearchWithProducerConsumer.java
@@ -4,13 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.multithreading.producerconsumerproblemwordsearch.models.*;
 
-import org.multithreading.producerconsumerproblemwordsearch.models.Producer;
-import org.multithreading.producerconsumerproblemwordsearch.models.WordLineNumberAndPos;
-import org.multithreading.producerconsumerproblemwordsearch.models.WordOccured;
-import org.multithreading.producerconsumerproblemwordsearch.models.WordSearchResult;
-
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -22,43 +22,74 @@ public class WordSearchWithProducerConsumer {
 
         File folder = new File("/home/karthikr_700073/Downloads/Karthik");
         String word = "the";
-        int numberOfConsumers = 5;
+        int[] consumerCounts = {1, 2, 4, 8, 16, 32};
+        File outputFile = new File("output.txt");
 
         BlockingQueue<String> queue = new LinkedBlockingQueue<>();
         List<String> fileList = new ArrayList<>();
         List<WordOccured> ans = new ArrayList<>();
         List<WordSearchResult> locationOfWord = new ArrayList<>();
 
-        Producer producer = new Producer(folder, queue, fileList);
-        Thread producerThread = new Thread(producer);
-        producerThread.start();
-        logger.info("Producer Thread Started");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            for (int numberOfConsumers : consumerCounts) {
+                long startTime = System.currentTimeMillis();
+                Producer producer = new Producer(folder, queue, fileList);
+                Thread producerThread = new Thread(producer);
+                producerThread.start();
+                logger.info("Producer Thread Started");
+                writer.write("Producer Thread Started\n");
 
-        Thread[] consumerThreads = new Thread[numberOfConsumers];
+                Thread[] consumerThreads = new Thread[numberOfConsumers];
 
-        for (int i = 0; i < numberOfConsumers; i++) {
-            consumerThreads[i] = new Thread(new Consumer(word, queue, ans, locationOfWord, producer));
-            consumerThreads[i].start();
-        }
-        logger.info( "consumer Threads Started");
-        producerThread.join();
-        logger.info( "producing the files from the folder is completed");
+                for (int i = 0; i < numberOfConsumers; i++) {
+                    consumerThreads[i] = new Thread(new Consumer(word, queue, ans, locationOfWord, producer));
+                    consumerThreads[i].start();
+                }
+                logger.info("Consumer Threads Started");
+                writer.write("Consumer Threads Started\n");
 
-        for (Thread thread : consumerThreads) {
-            thread.join();
-        }
-        logger.info( "consumer thereads are done");
+                producerThread.join();
+                logger.info("Producing the files from the folder is completed");
+                writer.write("Producing the files from the folder is completed\n");
 
-        System.out.println("List of all files: " + fileList);
+                for (Thread thread : consumerThreads) {
+                    thread.join();
+                }
+                logger.info("Consumer threads are done");
+                writer.write("Consumer threads are done\n");
 
-        System.out.println();
-        for (WordSearchResult list : locationOfWord) {
-            List<WordLineNumberAndPos> finalOutput = list.getLineNumberAndPos();
-            System.out.println(list.getAbsolutePath());
-            for (WordLineNumberAndPos out : finalOutput) {
-                System.out.println(out.getLineNumber() + " " + out.getPos());
+                long endTime = System.currentTimeMillis();
+                long duration = endTime - startTime;
+                logger.info("Execution Time with " + numberOfConsumers + " consumers: " + duration + " ms");
+                writer.write("Execution Time with " + numberOfConsumers + " consumers: " + duration + " ms\n");
             }
-            System.out.println();
+
+            Collections.sort(fileList);
+            writer.write("\nList of all files:\n");
+            for (String file : fileList) {
+                writer.write(file + "\n");
+            }
+
+            Collections.sort(ans, Comparator.comparing(WordOccured::getFile));
+            writer.write("\nWord Occurrences:\n");
+            for (WordOccured occurrences : ans) {
+                writer.write(occurrences.getFile() + " " + occurrences.getOccurred() + "\n");
+            }
+
+            Collections.sort(locationOfWord, Comparator.comparing(WordSearchResult::getAbsolutePath));
+            writer.write("\nWord Positions:\n");
+            for (WordSearchResult list : locationOfWord) {
+                List<WordLineNumberAndPos> finalOutput = list.getLineNumberAndPos();
+                writer.write(list.getAbsolutePath() + "\n");
+                for (WordLineNumberAndPos out : finalOutput) {
+                    writer.write(out.getLineNumber() + " " + out.getPos() + "\n");
+                }
+                writer.write("\n");
+            }
+            logger.info("Results successfully written to file: " + outputFile.getAbsolutePath());
+        } catch (IOException e) {
+            logger.error("Error writing to file", e);
         }
     }
 }
